d00c0fd6a14be6119b5105db012fc6b85f71917f
Valk-99 <valentinmirosnik0@gmail.com>
2021-06-09 09:53:03 +0300

    add django admin

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+
+import django
+
 from aiogram import executor
 
 from loader import dp
@@ -14,5 +18,15 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
+def setup_django():
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "django_admin.django_admin.settings"
+    )
+    os.environ.update({'DJANGO_ALLOW_ASYNC_UNSAFE': "true"})
+    django.setup()
+
+
 if __name__ == '__main__':
+    setup_django()
     executor.start_polling(dp, on_startup=on_startup)
--- data/config.py
+++ data/config.py
@@ -1,3 +1,5 @@
+import os
+
 from environs import Env
 
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
@@ -7,3 +9,10 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+
+PGUSER = str(os.getenv("PGUSER"))
+PGPASSWORD = str(os.getenv("PGPASSWORD"))
+DATABASE = str(os.getenv("DATABASE"))
+DBHOST = str(os.getenv("DBHOST"))
+
+POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{DBHOST}/{DATABASE}"
